1973c040cabd3ac1f1e474c22979ae417747f4f2
Pete J. Chiboleca <pete9450@gmail.com>
2020-12-21 10:12:29 +0200

    Paychecks Section - Part 1 Finished

--- Intermediate/PayRoll-E+/js/script.js
+++ Intermediate/PayRoll-E+/js/script.js
@@ -1,23 +1,34 @@
 // Data Structure Controller
-let DataController = (() => {
+const DataController = (() => {
   // Employee Function Constructor
-  let Employee = (id, name, age) => {
+  let Employee = (id, name, age, registrationDate) => {
     this.id = id;
     this.name = name;
     this.age = age;
+    this.registrationDate = registrationDate;
   };
 
-  console.log("Data Controller Working");
+  console.log("Data Controller Running");
 })();
 
 // User Interface Controller
-let UIController = (() => {
-  console.log("UI Controller Working");
+const UIController = (() => {
+  console.log("UI Controller Running");
 })();
 
 // Global Controller
-let Controller = ((dataCtrl, uiCtrl) => {
+const Controller = ((dataCtrl, uiCtrl) => {
+  console.log("Global Controller Running");
 
-  
+  document.querySelector(".item-1").addEventListener("click", () => {
+    document
+      .querySelector(".paychecks-section")
+      .classList.add("show-paychecks-section");
+  });
 
+  document.querySelector(".close-modal").addEventListener("click", () => {
+    document
+      .querySelector(".paychecks-section")
+      .classList.remove("show-paychecks-section");
+  });
 })(DataController, UIController);
