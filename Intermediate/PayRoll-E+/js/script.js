81c63b2dd564f25607291d374160c723978fe2f6
Pete J. Chiboleca <pete9450@gmail.com>
2021-01-23 15:57:27 +0200

    Added Salary property, fields and some fixes to the UI

--- Intermediate/PayRoll-E+/js/script.js
+++ Intermediate/PayRoll-E+/js/script.js
@@ -1,11 +1,25 @@
 // Data Structure Controller
 const DataController = (() => {
   // Employee Function Constructor
-  let Employee = (id, name, age, registrationDate) => {
+  let Employee = (id, name, age, registrationDate, salary) => {
     this.id = id;
     this.name = name;
     this.age = age;
     this.registrationDate = registrationDate;
+    this.salary = salary;
+  };
+
+  // Paycheck Function Constructor
+  let Paycheck = (id, employeeId, month, status = "Not Paid") => {
+    this.id = id;
+    this.employeeId = employeeId;
+    this.month = month;
+    this.status = status;
+  };
+
+  let data = {
+    emp: [],
+    paychecks: [],
   };
 
   console.log("Data Controller Running");
@@ -20,41 +34,54 @@ const UIController = (() => {
 const Controller = ((dataCtrl, uiCtrl) => {
   console.log("Global Controller Running");
 
-  document.querySelector(".item-1").addEventListener("click", () => {
-    document
-      .querySelector(".paychecks-section")
-      .classList.add("show-paychecks-section");
-  });
+  const setUpEventListeners = () => {
+    document.querySelector(".item-1").addEventListener("click", () => {
+      document
+        .querySelector(".paychecks-section")
+        .classList.add("show-paychecks-section");
+    });
 
-  document.querySelector(".close-modal").addEventListener("click", () => {
-    document
-      .querySelector(".paychecks-section")
-      .classList.remove("show-paychecks-section");
-  });
+    document.querySelector(".close-modal").addEventListener("click", () => {
+      document
+        .querySelector(".paychecks-section")
+        .classList.remove("show-paychecks-section");
+    });
 
-  document.querySelector(".btn-update-info").addEventListener("click", () => {
-    document
-      .querySelector(".employee-update")
-      .classList.add("show-employee-update");
-    document.querySelector(".paychecks-list").classList.add("move-list-down");
-    document
-      .querySelector(".employee-information")
-      .classList.add("scale-employee-information");
-  });
+    document.querySelector(".btn-update-info").addEventListener("click", () => {
+      document
+        .querySelector(".employee-update")
+        .classList.add("show-employee-update");
+      document.querySelector(".paychecks-list").classList.add("move-list-down");
+      document
+        .querySelector(".employee-information")
+        .classList.add("scale-employee-information");
+    });
 
-  document.querySelector(".btn-cancel").addEventListener("click", () => {
-    document
-      .querySelector(".employee-update")
-      .classList.remove("show-employee-update");
+    document.querySelector(".btn-cancel").addEventListener("click", () => {
+      document
+        .querySelector(".employee-update")
+        .classList.remove("show-employee-update");
 
       document
-          .querySelector(".employee-information")
-          .classList.remove("scale-employee-information");
-      });
+        .querySelector(".employee-information")
+        .classList.remove("scale-employee-information");
+    });
 
     document
       .querySelector(".employee-information")
-      .addEventListener("transitionend", () => {
-        
-  });
+      .addEventListener("transitionend", () => {});
+  };
+
+  return {
+    init: () => {
+      setUpEventListeners();
+      console.log("Application Started");
+    },
+
+    helloThere: () => {
+      console.log("Hello There");
+    },
+  };
 })(DataController, UIController);
+
+Controller.init();
